cd87be7d8553849f077bcc81a49c78c4c0dbc021
OrangePebble <git@orangepebble.net>
2026-07-07 10:11:31 +0100

    Improved cppdbg dap

--- lua/plugins/dev/dap.lua
+++ lua/plugins/dev/dap.lua
@@ -21,11 +21,110 @@ return {
 		},
 
 		config = function()
+			local pick_executable = function(path, title)
+				if path:sub(-1) ~= "/" then
+					path = path .. "/"
+				end
+				local co = coroutine.running()
+				local selected = nil
+				Snacks.picker.files({
+					cwd = path,
+					title = title or "Path to executable",
+					ignored = true,
+					hidden = true,
+					layout = { hidden = { "preview" } },
+					confirm = function(picker, item)
+						picker:close()
+						selected = item and item.file or nil
+						if co then
+							coroutine.resume(co)
+						end
+					end,
+				})
+				if co then
+					coroutine.yield()
+				end
+				return path .. selected
+			end
 			require("mason-nvim-dap").setup({
 				-- Makes a best effort to setup the various debuggers with reasonable debug configurations
 				automatic_installation = true,
 				-- Additional handler configuration.
-				handlers = {},
+				handlers = {
+					function(config)
+						-- all sources with no handler get passed here
+						-- Keep original functionality
+						require("mason-nvim-dap").default_setup(config)
+					end,
+					cppdbg = function(config)
+						-- Taken from https://github.com/jay-babu/mason-nvim-dap.nvim/blob/main/lua/mason-nvim-dap/mappings/configurations.lua
+						--  and modified.
+						local program_func = function()
+							local cwd_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+							if string.match(cwd_name, "ddad") then
+								return pick_executable(
+									vim.fn.getcwd() .. "/bazel-bin",
+									"Path to executable (/bazel-bin)"
+								)
+							else
+								return pick_executable(vim.fn.getcwd(), nil)
+							end
+						end
+						config.configurations = {
+							{
+								name = "Launch file",
+								type = "cppdbg",
+								request = "launch",
+								program = program_func,
+								cwd = "${workspaceFolder}",
+								stopAtEntry = true,
+							},
+							{
+								name = "Launch file (args)",
+								type = "cppdbg",
+								request = "launch",
+								program = program_func,
+								cwd = "${workspaceFolder}",
+								args = function()
+									return vim.split(vim.fn.input("Args: "), " +", { trimempty = true })
+								end,
+								stopAtEntry = true,
+							},
+							{
+								name = "Attach to gdbserver :1234",
+								type = "cppdbg",
+								request = "launch",
+								MIMode = "gdb",
+								miDebuggerServerAddress = "localhost:1234",
+								miDebuggerPath = vim.fn.exepath("gdb"),
+								cwd = "${workspaceFolder}",
+								program = program_func,
+							},
+							{
+								name = "Attach to gdbserver (port)",
+								type = "cppdbg",
+								request = "launch",
+								MIMode = "gdb",
+								miDebuggerServerAddress = function()
+									local uri = vim.fn.input("[host]:port : ")
+									if uri:find("^%d+$") == 1 then
+										uri = "localhost:" .. uri
+									elseif uri:find(":", nil, true) == 1 then
+										uri = "localhost" .. uri
+									end
+									return uri
+								end,
+								miDebuggerPath = vim.fn.exepath("gdb"),
+								cwd = "${workspaceFolder}",
+								args = function()
+									return vim.split(vim.fn.input("Args: "), " +", { trimempty = true })
+								end,
+								program = program_func,
+							},
+						}
+						require("mason-nvim-dap").default_setup(config) -- don't forget this!
+					end,
+				},
 				-- Get all possible values for here at:
 				--  https://github.com/jay-babu/mason-nvim-dap.nvim/blob/main/lua/mason-nvim-dap/mappings/source.lua
 				ensure_installed = {},
