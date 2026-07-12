422e0308c232ae2d3cee1d4dc3b6ad371c079b0f
OrangePebble <git@orangepebble.net>
2026-07-12 15:24:23 +0100

    Improved lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,17 +2,6 @@ return {
 	-- Fast and easy to configu statusline.
 	"nvim-lualine/lualine.nvim",
 	config = function()
-		local trouble = require("trouble")
-		local symbols = trouble.statusline({
-			mode = "lsp_document_symbols",
-			groups = {},
-			title = false,
-			filter = { range = true },
-			format = "{kind_icon}{symbol.name:Normal}",
-			-- The following line is needed to fix the background color
-			-- Set it to the lualine section you want to use
-			hl_group = "lualine_c_normal",
-		})
 		-- See the default config here:
 		-- https://github.com/nvim-lualine/lualine.nvim?tab=readme-ov-file#default-configuration
 		require("lualine").setup({
@@ -22,8 +11,8 @@ return {
 			extensions = {
 				"trouble",
 				"nvim-dap-ui",
+				{ sections = { lualine_y = { "filetype" } }, filetypes = { "help" } },
 				{
-					-- nvim-tree
 					sections = {
 						lualine_a = {
 							{
@@ -36,10 +25,53 @@ return {
 							},
 						},
 					},
+					inactive_sections = {
+						lualine_c = {
+							{
+								function()
+									return vim.fn.fnamemodify(vim.fn.getcwd(), ":~")
+								end,
+								on_click = function()
+									require("nvim-tree.api").tree.toggle()
+								end,
+							},
+						},
+					},
 					filetypes = { "NvimTree" },
+				},
+				{
+					sections = {
+						lualine_a = {
+							function()
+								-- Returning just the filetype icon because the window is usually very thin.
+								return "󰜎"
+							end,
+						},
+					},
+					inactive_sections = {
+						lualine_c = {
+							function()
+								-- Returning just the filetype icon because the window is usually very thin.
+								return "󰜎"
+							end,
+						},
+					},
+					filetypes = { "OverseerList" },
+				},
+				{
+					sections = {
+						lualine_c = { { "overseer", symbols = { [require("overseer").STATUS.PENDING] = "󱎫 " } } },
+						lualine_z = { "filetype" },
+					},
+					inactive_sections = {
+						lualine_c = { { "overseer", symbols = { [require("overseer").STATUS.PENDING] = "󱎫 " } } },
+						lualine_x = { "filetype" },
+					},
+					filetypes = { "OverseerOutput" },
 				},
 			},
 			sections = {
+				lualine_a = { "mode" },
 				lualine_b = {
 					"diff",
 					{
@@ -51,20 +83,63 @@ return {
 							hint = " ",
 						},
 					},
-					{ "overseer", symbols = {
-						[require("overseer").STATUS.PENDING] = "󱎫 ",
-					} },
-				},
-				lualine_c = {
 					{
-						symbols.get,
+						"overseer",
+						symbols = {
+							[require("overseer").STATUS.PENDING] = "󱎫 ",
+						},
 						cond = function()
-							return symbols.has() and (vim.b.has_lsp == true)
+							for _, win in ipairs(vim.api.nvim_list_wins()) do
+								local buf = vim.api.nvim_win_get_buf(win)
+								if vim.bo[buf].filetype == "OverseerOutput" then
+									return false
+								end
+							end
+							return true
 						end,
 					},
 				},
+				lualine_c = {},
 				lualine_x = {},
 				lualine_y = { "filetype" },
+				lualine_z = {
+					-- Added this so I can easily tell if I've closed the wrong file during a git diff but
+					--  it might also be useful in other occasions.
+					{
+						function()
+							return vim.bo.buftype
+						end,
+						cond = function()
+							return vim.bo.buftype ~= ""
+						end,
+					},
+					{
+						"filename",
+						on_click = function()
+							require("nvim-tree.api").tree.toggle({ focus = false })
+						end,
+						cond = function()
+							return not vim.g.nvim_tree_open
+						end,
+					},
+					{
+						"filename",
+						path = 1, -- Relative path
+						on_click = function()
+							require("nvim-tree.api").tree.close()
+						end,
+						cond = function()
+							return vim.g.nvim_tree_open
+						end,
+					},
+				},
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
 				lualine_z = {
 					-- Added this so I can easily tell if I've closed the wrong file during a git diff but
 					--  it might also be useful in other occasions.
