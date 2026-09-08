bff4bf9f4923baad78aa81778c8a4a20adf10d0a
OrangePebble <git@orangepebble.net>
2026-09-08 13:55:13 +0100

    Made the new otter get_clients filter work better with snacks

--- lua/plugins/dev/otter.lua
+++ lua/plugins/dev/otter.lua
@@ -12,22 +12,11 @@ return {
 		-- Route LSP requests to Otter only within an injected language region.
 		-- Required because I apply Otter in all buffers below, and without this, many LSP requests are called twice.
 		local original_get_clients = vim.lsp.get_clients
-		---@diagnostic disable-next-line: duplicate-set-field
-		vim.lsp.get_clients = function(opts)
-			local clients = original_get_clients(opts)
-			local bufnr = opts and opts.bufnr
-
-			-- Otter does not inject workspace-wide operations, so just return all clients.
-			if not bufnr then
-				return clients
-			end
-
-			if bufnr == 0 then
-				bufnr = vim.api.nvim_get_current_buf()
-			end
+		local snacks_lsp = require("snacks.picker.source.lsp")
+		local snacks_get_clients = snacks_lsp.get_clients
+		local function filter_otter_clients(clients, bufnr)
 			local otter_client_name = "otter-ls[" .. bufnr .. "]"
-
-			-- If Otter is not applied in the current buffer, return all clients.
+			-- If this request has no Otter client for the buffer, return all clients.
 			if
 				not vim.tbl_contains(
 					vim.tbl_map(function(client)
@@ -38,15 +27,37 @@ return {
 			then
 				return clients
 			end
-
 			-- Filter out Otter clients if the cursor is not at an injected language, and non-Otter clients if it is.
 			return vim.tbl_filter(function(client)
 				return (client.name == otter_client_name) == (otter_keeper.get_current_language_context(bufnr) ~= nil)
 			end, clients)
 		end
 
+		---@diagnostic disable-next-line: duplicate-set-field
+		vim.lsp.get_clients = function(opts)
+			local clients = original_get_clients(opts)
+			local bufnr = opts and opts.bufnr
+
+			-- Otter does not inject workspace-wide operations, so just return all clients.
+			-- If no method is specified (like Snacks and statuslines do), also return all clients.
+			if not bufnr or not opts.method then
+				return clients
+			end
+
+			if bufnr == 0 then
+				bufnr = vim.api.nvim_get_current_buf()
+			end
+			return filter_otter_clients(clients, bufnr)
+		end
+
+		-- Snacks filters method support after calling vim.lsp.get_clients without a method, so we need to filter Otter after.
+		---@diagnostic disable-next-line: duplicate-set-field
+		snacks_lsp.get_clients = function(bufnr, method)
+			return filter_otter_clients(snacks_get_clients(bufnr, method), bufnr)
+		end
+
 		-- Automatically activate Otter for normal file buffers with an available Tree-sitter parser.
-		-- Otter only activates for files with injected languages.
+		-- Otter only creates hidden buffers and attaches its LSP when injected languages are found.
 		vim.api.nvim_create_autocmd("FileType", {
 			group = vim.api.nvim_create_augroup("otter.activate", {}),
 			pattern = "*",
